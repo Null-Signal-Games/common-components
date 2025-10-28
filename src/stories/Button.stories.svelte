0428ceece8a9bd1f5ae7d8a0c170b8315164f2f8
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2025-10-28 17:01:26 -0500

    basic story

--- src/stories/Button.stories.svelte
+++ src/stories/Button.stories.svelte
@@ -1,31 +1,31 @@
 <script module>
-  import { defineMeta } from '@storybook/addon-svelte-csf';
-  import Button from './Button.svelte';
-  import { fn } from 'storybook/test';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Button from '$lib/Button.svelte';
+	import { fn } from 'storybook/test';
 
-  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-  const { Story } = defineMeta({
-    title: 'Example/Button',
-    component: Button,
-    tags: ['autodocs'],
-    argTypes: {
-      backgroundColor: { control: 'color' },
-      size: {
-        control: { type: 'select' },
-        options: ['small', 'medium', 'large'],
-      },
-    },
-    args: {
-      onclick: fn(),
-    }
-  });
+	const sampleAlert = () => {
+		window.alert('button!');
+	};
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Button',
+		component: Button,
+		tags: ['autodocs'],
+		argTypes: {
+			disabled: { control: 'boolean' },
+			children: {
+				control: 'text'
+			}
+		},
+		args: {
+			disabled: false,
+			onclick: fn(),
+			children: 'button'
+		}
+	});
 </script>
 
-<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
-<Story name="Primary" args={{ primary: true, label: 'Button' }} />
+<Story name="Primary" args={{ children: 'Button', onclick: sampleAlert }} />
 
-<Story name="Secondary" args={{ label: 'Button' }} />
-
-<Story name="Large" args={{ size: 'large', label: 'Button' }} />
-
-<Story name="Small" args={{ size: 'small', label: 'Button' }} />
+<Story name="Disabled" args={{ children: 'Button', disabled: true, onclick: sampleAlert }} />
